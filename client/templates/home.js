2d33da55727a087c51db5917bc371beadcb172dc
Alex Chan <alexkchan@gmail.com>
2015-02-12 21:43:05 -0800

    Added buttons from home page to add to wellness index page. Refresh after each button click to get rid of scrolling bug.

---
+++ client/templates/home.js
@@ -0,0 +1,27 @@
+Template.home.helpers({ 
+
+  // addPost: function()
+  // {
+  //   var post = {
+  //       url: 'img/stepbystep.jpg',
+  //       title: 'Step by Step'
+  //     };
+      
+  //     var errors = validatePost(post);
+  //     if (errors.title || errors.url)
+  //       return Session.set('postSubmitErrors', errors);
+      
+  //     Meteor.call('postInsert', post, function(error, result) {
+  //       // display the error to the user and abort
+  //       if (error)
+  //         return throwError(error.reason);
+        
+  //       // show this result but route anyway
+  //       if (result.postExists)
+  //         throwError('This link has already been posted');
+        
+  //       Router.go('/new');
+  //     });
+  // }
+
+});
